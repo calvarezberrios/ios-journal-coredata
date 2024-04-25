99b6e5f8eba2745640a254f8b7728a9ecaa8d463
Carlos "Mannie" Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2024-04-25 15:17:47 -0400

    project created, storyboard designed, data model created, data model for entry initialized in entry+convenience

---
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -0,0 +1,19 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Carlos E. Alvarez-Berrios on 4/25/24.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(id: String = UUID().uuidString, title: String, bodyText: String, timeStamp: Date = Date(), context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.id = id
+        self.title = title
+        self.bodyText = bodyText
+        self.timeStamp = timeStamp
+    }
+}
---
+++ Journal/Journal/View Controllers/CreateEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateEntryViewController.swift
+//  Journal
+//
+//  Created by Carlos E. Alvarez-Berrios on 4/25/24.
+//
+
+import UIKit
+
+class CreateEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
