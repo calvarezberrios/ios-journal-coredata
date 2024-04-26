3d28e55e67e40c06fc9c5726398fe71eb47dee80
Carlos "Mannie" Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2024-04-26 08:06:47 -0400

    Added mood version to data model, added mood property to model, created enum for mood in entry+convenience and added mood to initializer

--- Journal/Journal/Models/Entry+Convenience.swift
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -8,11 +8,18 @@
 import Foundation
 import CoreData
 
+enum Mood: String, CaseIterable {
+    case happy = "😀"
+    case neutral = "😐"
+    case sad = "😞"
+}
+
 extension Entry {
-    @discardableResult convenience init(id: String = UUID().uuidString, title: String, bodyText: String, timeStamp: Date = Date(), context: NSManagedObjectContext) {
+    @discardableResult convenience init(id: String = UUID().uuidString, title: String, mood: Mood, bodyText: String, timeStamp: Date = Date(), context: NSManagedObjectContext) {
         self.init(context: context)
         self.id = id
         self.title = title
+        self.mood = mood.rawValue
         self.bodyText = bodyText
         self.timeStamp = timeStamp
     }
